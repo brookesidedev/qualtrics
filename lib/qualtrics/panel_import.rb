72ceb8731ce5d0e68d1926d0c45a617abfc2d880
Kevin Sun <sunkev@brandeis.edu>
2016-03-28 15:49:11 -0400

    adds headers length back in

--- lib/qualtrics/panel_import.rb
+++ lib/qualtrics/panel_import.rb
@@ -34,7 +34,7 @@ module Qualtrics
       payload['ColumnHeaders'] = 1
 
       if @embedded_data_columns.to_i > 0
-        payload['EmbeddedData'] = ((headers_length + 1).. (headers_length + @embedded_data_columns.to_i)).to_a.join(',')
+        payload['EmbeddedData'] = ((headers.length + 1).. (headers.length + @embedded_data_columns.to_i)).to_a.join(',')
       end
 
       payload
